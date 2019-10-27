dce9a2d41f1b6b5a84ee5220b4f808ef8c98d5f7
YangLi <526214077@qq.com>
2019-10-27 17:20:00 +0800

    Li_Yang_lab5

---
+++ app/Models/Places.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Places
+ *
+ * @author lenovo
+ */
+class Places {
+    //put your code here
+    protected $origin = WRITEPATH . '/data/xxx/Place.csv';
+    protected $keyField = 'id';
+    protected $validationRles = [];
+}
---
+++ writable/logs/log-2019-10-27.php
@@ -0,0 +1,93 @@
+<?php defined('SYSTEMPATH') || exit('No direct script access allowed'); ?>
+
+CRITICAL - 2019-10-27 03:06:09 --> Class 'App\Models\Places' not found
+#0 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Travel->index()
+#1 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Travel))
+#2 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\xampp\htdocs\comp4711-lab5\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\xampp\\htdocs...')
+#5 {main}
+CRITICAL - 2019-10-27 03:06:12 --> Class 'App\Models\Places' not found
+#0 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Places->index()
+#1 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Places))
+#2 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\xampp\htdocs\comp4711-lab5\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\xampp\\htdocs...')
+#5 {main}
+CRITICAL - 2019-10-27 04:04:38 --> Class 'App\Models\Places' not found
+#0 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Travel->index()
+#1 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Travel))
+#2 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\xampp\htdocs\comp4711-lab5\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\xampp\\htdocs...')
+#5 {main}
+CRITICAL - 2019-10-27 04:04:41 --> Class 'App\Models\Places' not found
+#0 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Places->index()
+#1 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Places))
+#2 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\xampp\htdocs\comp4711-lab5\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\xampp\\htdocs...')
+#5 {main}
+CRITICAL - 2019-10-27 04:04:57 --> Class 'App\Models\Places' not found
+#0 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Travel->index()
+#1 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Travel))
+#2 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\xampp\htdocs\comp4711-lab5\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\xampp\\htdocs...')
+#5 {main}
+CRITICAL - 2019-10-27 04:04:59 --> Class 'App\Models\Places' not found
+#0 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Places->index()
+#1 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Places))
+#2 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\xampp\htdocs\comp4711-lab5\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\xampp\\htdocs...')
+#5 {main}
+CRITICAL - 2019-10-27 04:05:31 --> Class 'App\Models\Places' not found
+#0 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Travel->index()
+#1 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Travel))
+#2 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\xampp\htdocs\comp4711-lab5\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\xampp\\htdocs...')
+#5 {main}
+CRITICAL - 2019-10-27 04:06:19 --> Class 'App\Models\Places' not found
+#0 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Places->index()
+#1 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Places))
+#2 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\xampp\htdocs\comp4711-lab5\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\xampp\\htdocs...')
+#5 {main}
+CRITICAL - 2019-10-27 04:06:20 --> Class 'App\Models\Places' not found
+#0 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Travel->index()
+#1 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Travel))
+#2 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\xampp\htdocs\comp4711-lab5\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\xampp\\htdocs...')
+#5 {main}
+CRITICAL - 2019-10-27 04:07:18 --> Class 'App\Models\Places' not found
+#0 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Travel->index()
+#1 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Travel))
+#2 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\xampp\htdocs\comp4711-lab5\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\xampp\\htdocs...')
+#5 {main}
+CRITICAL - 2019-10-27 04:08:11 --> Class 'App\Models\Places' not found
+#0 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Travel->index()
+#1 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Travel))
+#2 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\xampp\htdocs\comp4711-lab5\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\xampp\\htdocs...')
+#5 {main}
+CRITICAL - 2019-10-27 04:09:06 --> Class 'App\Models\Places' not found
+#0 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Travel->index()
+#1 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Travel))
+#2 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\xampp\htdocs\comp4711-lab5\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\xampp\\htdocs...')
+#5 {main}
+CRITICAL - 2019-10-27 04:13:29 --> Class 'App\Models\Places' not found
+#0 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Places->index()
+#1 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Places))
+#2 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\xampp\htdocs\comp4711-lab5\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\xampp\htdocs\comp4711-lab5\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\xampp\\htdocs...')
+#5 {main}
