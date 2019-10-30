e6cb45c385cad0b7c363185ff635000a050e3e6a
YangLi <526214077@qq.com>
2019-10-30 09:32:39 +0800

    lab5_LiYang

--- app/Models/Places.php
+++ app/Models/Places.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace App\Models;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,9 +11,9 @@
  *
  * @author lenovo
  */
-class Places {
+class Places extends Simple\CSVModel{
     //put your code here
-    protected $origin = WRITEPATH . '/data/Place.csv';
+    protected $origin = WRITEPATH . '/data/Places.csv';
     protected $keyField = 'id';
     protected $validationRles = [];
 }
