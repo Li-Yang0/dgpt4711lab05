c7d3eabf79a36085ec8f4550e654f87ca989e0c1
YangLi <526214077@qq.com>
2019-11-03 18:43:48 +0800

    Li_Yang_lab6

--- app/Controllers/Travel.php
+++ app/Controllers/Travel.php
@@ -6,16 +6,38 @@ class Travel extends BaseController
     public function index()
     {
         // connect to the model
-    $places = new \App\Models\Places();
-        // retrieve all the records
-    $records = $places->findAll();
+//    $places = new \App\Models\Places();
+//        // retrieve all the records
+//    $records = $places->findAll();
+//    
+//        // get a template parser
+//    $parser = \Config\Services::parser();
+//        // tell it about the substitions
+//    return $parser->setData(['records' => $records])
+//        // and have it render the template with those
+//    ->render('placeslist');
     
-        // get a template parser
-    $parser = \Config\Services::parser();
-        // tell it about the substitions
-    return $parser->setData(['records' => $records])
-        // and have it render the template with those
-    ->render('placeslist');
+        $model = new \App\Models\Places();
+        $headings = $model->fields;
+        $data = $model->findAll();
+
+        $table = new \CodeIgniter\View\Table();
+        // drop the last heading column
+        unset($headings[count($headings)-1]);
+        $table->setHeading($headings);
+
+        foreach($data as $record) {
+        //  unset($record[count($record)-1]);
+        //  $table-addRow($record);
+                $table->addRow($record->id, $record->name, $record->description, $record->link);
+        }
+
+        $content = $table->generate();
+        
+        $parser = \Config\Services::parser();
+        $output = $parser->render('top').$content.$parser->render('bottom');
+        return $output;
+        
     }
     public function showme($id)
     {
